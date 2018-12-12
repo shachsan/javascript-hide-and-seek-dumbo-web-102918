7d7bb05d16880d8ff6ebb10be5d28619b0a5460a
Sanjay Sai <shachsan@hotmail.com>
2018-12-11 23:01:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+getFirstSelector = (selector)=>{
+  return document.querySelector(selector);
+}
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
 
+function deepestChild(){
+  let grandNodes = document.querySelectorAll('#grand-node div');
+  return grandNodes[grandNodes.length - 1];
+}
+
+function increaseRankBy(n){
+  let rankLists = document.querySelectorAll('.ranked-list li')
+  for (let list of rankLists){
+    // console.log(list);
+    list.innerHTML = parseInt(list.innerHTML)+n;
+  }
+}
